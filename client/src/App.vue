8aea95e356bf6890d7775db629bd7dac2d4535ac
Faris Widyantho <radenfaris@gmail.com>
2017-11-29 17:22:41 +0700

    update fb button

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  created () {
+    
+  }
 }
 </script>
 
---
+++ client/src/components/Home.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+  <button type="button" name="button">Facebook Login</button>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
