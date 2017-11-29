64eaa5297bf6da4185af01cfb11a31f62b003f16
chandrabuwana <cbuwana@gmail.com>
2017-11-29 17:15:34 +0700

    navbar room sama firebase  config sudah masuk

---
+++ client/src/components/Room.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-md-6">
+        <h1>hokya</h1>
+      </div>
+      <div class="col-md-6">
+        <h1>hayoo</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
